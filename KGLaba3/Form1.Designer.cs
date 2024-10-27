afaba6558da27c42230edd4cafb802980b32cf9d
Electr1k <gynter37@gmail.com>
2024-10-27 21:14:52 +0300

    ya3

--- KGLaba3/Form1.Designer.cs
+++ KGLaba3/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(376, 385);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Paint += PictureBox1_Paint;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(446, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(376, 385);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            pictureBox2.Paint += PictureBox2_Paint;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(851, 488);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
     }
 }
--- KGLaba3/Form1.cs
+++ KGLaba3/Form1.cs
@@ -1,10 +1,112 @@
+using System.Drawing;
+
 namespace KGLaba3
 {
     public partial class Form1 : Form
     {
+        List<Pixel> pixelOutLineA = new List<Pixel>();
+        List<Pixel> pixelOutLineB = new List<Pixel>();
+        List<Pixel> pixelOutLineReference = new List<Pixel>();
+
         public Form1()
         {
             InitializeComponent();
+            this.PaintLineMain(3, 3, 100, 100, Color.Red);
+            this.PaintLineCDA(3, 3, 100, 100, Color.Red);
+            this.PaintLineBrezenthema(3, 3, 100, 100, Color.Red);
+
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            for(int i = 0; i < pixelOutLineA.Count(); i++){ 
+                graphics.FillRectangle(new SolidBrush(pixelOutLineA[i].color), pixelOutLineA[i].x, pixelOutLineA[i].y, 1, 1);
+            }
+        }
+
+        private void PictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            for (int i = 0; i < pixelOutLineReference.Count(); i++)
+            {
+                graphics.FillRectangle(new SolidBrush(pixelOutLineReference[i].color), pixelOutLineReference[i].x, pixelOutLineReference[i].y, 1, 1);
+            }
+        }
+
+
+        void PaintLineMain(int x1, int y1, int x2, int y2, Color color)
+        {
+            float dy = Math.Abs((y2 - y1) / (x2 - x1));
+            int x = x1;
+            float y = y1;
+            while (x <= x2)
+            {
+                pixelOutLineA.Add(new Pixel(x, (int)Math.Round(y, 0), color));
+                x++;
+                y += dy;
+            }
+        }
+
+        void PaintLineCDA(int x1, int y1, int x2, int y2, Color color)
+        {
+            int length = Math.Max(Math.Abs(x2 - x1), Math.Abs(y2 - y1));
+            float dx = (x2 - x1) / length;
+            float dy = (y2 - y1) / length;
+            float x = x1;
+            float y = y1;
+
+            int i = 1;
+            while (i <= length)
+            {
+                pixelOutLineB.Add(new Pixel((int)Math.Round(x, 0), (int)Math.Round(y, 0), color));
+                x += dx;
+                y += dy;
+                i++;
+            }
+        }
+
+        void PaintLineBrezenthema(int x1, int y1, int x2, int y2, Color color)
+        {
+            int x = x1;
+            int y = y1;
+
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+            int D = -dx;
+
+            int DX = dx << 1;
+            int DY = dy << 1;
+            while (x <= x2)
+            {
+                pixelOutLineReference.Add(new Pixel(x, y, color));
+
+                x++;
+                D += DY;
+                if (D >= 0)
+                {
+                    y++;
+                    D -= DX;
+                }
+            }
+        }
+
+        int Sign(float value)
+        {
+            return value > 0 ? 1 : (value < 0 ? -1 : 0);
+        }
+    }
+
+    public class Pixel
+    {
+        public int x, y;
+        public Color color;
+
+        public Pixel(int x, int y, Color color)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = color;
         }
     }
 }
