4c725ed63d9a5462b72a222911452278182c4ca6
Electr1k <gynter37@gmail.com>
2024-10-28 21:55:57 +0300

    t

--- KGLaba3/Form1.Designer.cs
+++ KGLaba3/Form1.Designer.cs
@@ -30,8 +30,10 @@
         {
             pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
@@ -52,17 +54,28 @@
             pictureBox2.TabStop = false;
             pictureBox2.Paint += PictureBox2_Paint;
             // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(884, 12);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(376, 385);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            pictureBox3.Paint += PictureBox3_Paint;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(851, 488);
+            ClientSize = new Size(1342, 488);
+            Controls.Add(pictureBox3);
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
         }
 
@@ -70,5 +83,6 @@
 
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
     }
 }
--- KGLaba3/Form1.cs
+++ KGLaba3/Form1.cs
@@ -23,7 +23,7 @@ namespace KGLaba3
         public Form1()
         {
             InitializeComponent();
-            //getPixelsReference();
+            getPixelsReference();
             getPixelsA();
             getPixelsB();
         }
@@ -32,11 +32,11 @@ namespace KGLaba3
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            
+
             this.PaintLineMain(-4, 1, -2, 1, Color.Green);
             this.PaintLineMain(-2, 1, -3, 6, Color.Green);
             this.PaintLineMain(-4, 1, -3, 6, Color.Green);
-            
+
 
             this.PaintLineMain(-2.5, 0, -2.5, 1.5, Color.Orange);
             this.PaintLineMain(-2.5, 1.5, 0.5, 1.5, Color.Orange);
@@ -72,29 +72,31 @@ namespace KGLaba3
 
             this.PaintLineMain(-3, -2, -2.5, 2.5, Color.Black);
             this.PaintLineMain(-3, -2, -3.5, 2.5, Color.Black);
-            
+
             stopwatch.Stop();
             TimeSpan elapsedTime = stopwatch.Elapsed;
             timeA = elapsedTime.TotalMilliseconds;
 
-            //List<Pixel> diff = CalculateDiff(pixelOutLineA, new List<Pixel>(), pixelOutLineReference, new List<Pixel>());
-            //countDiffA = diff.Count();
+            List<Pixel> diff = CalculateDiff(pixelOutLineA, new List<Pixel>(), pixelOutLineReference, new List<Pixel>());
+            countDiffA = diff.Count();
+            Console.WriteLine("Diff A: " + diff.Count() + " Time A: " + timeA + "Count total: " + pixelOutLineA.Count());
+
         }
 
         void getPixelsB()
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            
+
             this.PaintLineCDA(-4, 1, -2, 1, Color.Green);
             this.PaintLineCDA(-2, 1, -3, 6, Color.Green);
             this.PaintLineCDA(-4, 1, -3, 6, Color.Green);
-            
+
             this.PaintLineCDA(-2.5, 0, -2.5, 1.5, Color.Orange);
             this.PaintLineCDA(-2.5, 1.5, 0.5, 1.5, Color.Orange);
             this.PaintLineCDA(0.5, 1.5, 0.5, 0, Color.Orange);
             this.PaintLineCDA(0.5, 0, -2.5, 0, Color.Orange);
-            
+
             this.PaintLineCDA(0, 2.5, -0.5, 3, Color.LightYellow);
             this.PaintLineCDA(-0.5, 3, -1, 3, Color.LightYellow);
             this.PaintLineCDA(-1, 3, -1.5, 2.5, Color.LightYellow);
@@ -124,12 +126,12 @@ namespace KGLaba3
 
             this.PaintLineCDA(-3, -2, -2.5, 2.5, Color.Black);
             this.PaintLineCDA(-3, -2, -3.5, 2.5, Color.Black);
-            
+
             stopwatch.Stop();
             TimeSpan elapsedTime = stopwatch.Elapsed;
             timeB = elapsedTime.TotalMilliseconds;
-            //List<Pixel> diff = CalculateDiff(pixelOutLineB, new List<Pixel>(), pixelOutLineReference, new List<Pixel>());
-            //Console.WriteLine("Diff B: " + diff.Count() + " Time B: " + timeB + "Count total: " + pixelOutLineB.Count());
+            List<Pixel> diff = CalculateDiff(pixelOutLineB, new List<Pixel>(), pixelOutLineReference, new List<Pixel>());
+            Console.WriteLine("Diff B: " + diff.Count() + " Time B: " + timeB + "Count total: " + pixelOutLineB.Count());
         }
 
         void getPixelsReference()
@@ -137,11 +139,49 @@ namespace KGLaba3
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            this.PaintLineBrezenthema(3, 3, 100, 100, Color.Red);
+            this.PaintLineBrezenthema(-4, 1, -2, 1, Color.Green);
+            this.PaintLineBrezenthema(-2, 1, -3, 6, Color.Green);
+            this.PaintLineBrezenthema(-4, 1, -3, 6, Color.Green);
+            /*
+            this.PaintLineBrezenthema(-2.5, 0, -2.5, 1.5, Color.Orange);
+            this.PaintLineBrezenthema(-2.5, 1.5, 0.5, 1.5, Color.Orange);
+            this.PaintLineBrezenthema(0.5, 1.5, 0.5, 0, Color.Orange);
+            this.PaintLineBrezenthema(0.5, 0, -2.5, 0, Color.Orange);
+
+            this.PaintLineBrezenthema(0, 2.5, -0.5, 3, Color.LightYellow);
+            this.PaintLineBrezenthema(-0.5, 3, -1, 3, Color.LightYellow);
+            this.PaintLineBrezenthema(-1, 3, -1.5, 2.5, Color.LightYellow);
+            this.PaintLineBrezenthema(-1.5, 2.5, -1.5, 2, Color.LightYellow);
+            this.PaintLineBrezenthema(-1.5, 2, -1, 1.5, Color.LightYellow);
+            this.PaintLineBrezenthema(-1, 1.5, -0.5, 1.5, Color.LightYellow);
+            this.PaintLineBrezenthema(-0.5, 1.5, -4, 1, Color.LightYellow);
+
+            this.PaintLineBrezenthema(-2.5, 1.5, -1, 2.5, Color.Red);
+            this.PaintLineBrezenthema(-1, 2.5, 0.5, 1.5, Color.Red);
+            this.PaintLineBrezenthema(0.5, 1.5, -2.5, 1.5, Color.Red);
+            this.PaintLineBrezenthema(-1.5, 0.5, -1.5, 1, Color.Yellow);
+            this.PaintLineBrezenthema(-1.5, 1, -1, 1, Color.Yellow);
+            this.PaintLineBrezenthema(-1, 1, -1, 0.5, Color.Yellow);
+            this.PaintLineBrezenthema(-1, 0.5, -1.5, 0.5, Color.Yellow);
+
+            this.PaintLineBrezenthema(-0.5, 0, -0.5, 1, Color.SaddleBrown);
+            this.PaintLineBrezenthema(-0.5, 1, 0, 1, Color.SaddleBrown);
+            this.PaintLineBrezenthema(0, 1, 0, 0, Color.SaddleBrown);
+            this.PaintLineBrezenthema(0, 0, -0.5, 0, Color.SaddleBrown);
+
+            this.PaintLineBrezenthema(-3, 0, -3, 4, Color.Black);
+
+            this.PaintLineBrezenthema(-3, -1.5, -2.5, -2, Color.Black);
+            this.PaintLineBrezenthema(-3, -1.5, -3.5, -2, Color.Black);
+
+            this.PaintLineBrezenthema(-3, -2, -2.5, 2.5, Color.Black);
+            this.PaintLineBrezenthema(-3, -2, -3.5, 2.5, Color.Black);*/
 
             stopwatch.Stop();
             TimeSpan elapsedTime = stopwatch.Elapsed;
             timeMain = elapsedTime.TotalMilliseconds;
+            Console.WriteLine(" Time Reference: " + timeMain + " Count total: " + pixelOutLineReference.Count());
+
         }
 
         List<Pixel> CalculateDiff(List<Pixel> outlineT, List<Pixel> onsideT, List<Pixel> outlineM, List<Pixel> onsideM)
@@ -173,8 +213,9 @@ namespace KGLaba3
         private void PictureBox1_Paint(object sender, PaintEventArgs e)
         {
             Graphics graphics = e.Graphics;
-            for (int i = 0; i < pixelOutLineA.Count(); i++){ 
-                graphics.FillRectangle(new SolidBrush(pixelOutLineA[i].color), (pixelOutLineA[i].x + offsetXA) * scaleA, (pixelOutLineA[i].y + offsetYA) * scaleA, 1 * scaleA, 1 * scaleA);
+            for (int i = 0; i < pixelOutLineA.Count(); i++)
+            {
+                graphics.FillRectangle(new SolidBrush(pixelOutLineA[i].color), (pixelOutLineA[i].x + offsetXA * scaleA), (pixelOutLineA[i].y + offsetYA * scaleA), 1 * scaleA, 1 * scaleA);
             }
         }
 
@@ -183,17 +224,26 @@ namespace KGLaba3
             Graphics graphics = e.Graphics;
             for (int i = 0; i < pixelOutLineB.Count(); i++)
             {
-                graphics.FillRectangle(new SolidBrush(pixelOutLineB[i].color), (pixelOutLineB[i].x + offsetXB ) * scaleB, (pixelOutLineB[i].y + offsetYB) * scaleB, 1 * scaleB, 1 * scaleB);
+                graphics.FillRectangle(new SolidBrush(pixelOutLineB[i].color), (pixelOutLineB[i].x + offsetXB * scaleB), (pixelOutLineB[i].y + offsetYB * scaleB), 1 * scaleB, 1 * scaleB);
+            }
+        }
+
+        private void PictureBox3_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            for (int i = 0; i < pixelOutLineReference.Count(); i++)
+            {
+                graphics.FillRectangle(new SolidBrush(pixelOutLineReference[i].color), (pixelOutLineReference[i].x + offsetXB * scaleB), (pixelOutLineReference[i].y + offsetYB * scaleB), 1, 1);
             }
         }
 
 
         void PaintLineMain(double x1, double y1, double x2, double y2, Color color)
         {
-            /*x1 *= scaleA;
+            x1 *= scaleA;
             x2 *= scaleA;
             y1 *= scaleA;
-            y2 *= scaleA;*/
+            y2 *= scaleA;
             double dx = x2 - x1;
             double dy = y2 - y1;
 
@@ -206,33 +256,33 @@ namespace KGLaba3
             if (absDx > absDy)
             {
                 double y = y1;
-                double k = (double)dy / dx;  
+                double k = (double)dy / dx;
 
-                for (double x = x1; x < x2 + signX; x += signX)
+                for (double x = x1; x != x2 + signX; x += signX)
                 {
                     pixelOutLineA.Add(new Pixel((int)x, (int)Math.Round(y), color));
-                    y += k * signX; 
+                    y += k * signX;
                 }
             }
             else
             {
                 double x = x1;
-                double k = (double)dx / dy;  
+                double k = (double)dx / dy;
 
-                for (double y = y1; y < y2 + signY; y += signY)
+                for (double y = y1; y != y2 + signY; y += signY)
                 {
                     pixelOutLineA.Add(new Pixel((int)Math.Round(x), (int)y, color));
-                    x += k * signY; 
+                    x += k * signY;
                 }
             }
         }
 
         void PaintLineCDA(double x1, double y1, double x2, double y2, Color color)
         {
-            /*x1 *= scaleB;
+            x1 *= scaleB;
             x2 *= scaleB;
             y1 *= scaleB;
-            y2 *= scaleB;*/
+            y2 *= scaleB;
             double length = Math.Max(Math.Abs(x2 - x1), Math.Abs(y2 - y1));
             double dx = (x2 - x1) / length;
             double dy = (y2 - y1) / length;
@@ -255,33 +305,42 @@ namespace KGLaba3
             x2 *= scaleReference;
             y1 *= scaleReference;
             y2 *= scaleReference;
-            double x = x1;
-            double y = y1;
-
-            double dx = x2 - x1;
-            double dy = y2 - y1;
-            double D = -dx;
-
-            double DX = (int) dx << 1;
-            double DY = (int) dy << 1;
-            while (x <= x2)
+            if (x1 > x2)
             {
-                pixelOutLineReference.Add(new Pixel((int)x, (int)y, color));
+                double temp = x1;
+                x1 = x2;
+                x2 = temp;
 
-                x++;
-                D += DY;
-                if (D >= 0)
-                {
-                    y++;
-                    D -= DX;
-                }
+                temp = y1;
+                y1 = y2;
+                y2 = temp;
             }
+
+
+            double dx, dy, p, x, y;
+            dx = x2 - x1;
+            dy = y2 - y1;
+            x = x1;
+            y = y1;
+            p = 2 * dy - dx;
+            while (x < x2)
+            {
+                if (p >= 0)
+                {
+                    pixelOutLineReference.Add(new Pixel((int)x, (int)y, color));
+                    y = y - 1;
+                    p = p + 2 * dy - 2 * dx;
+                }
+                else
+                {
+                    pixelOutLineReference.Add(new Pixel((int)x, (int)y, color));
+                    p = p + 2 * dy;
+                }
+                x = x + 1;
+            }
+
         }
 
-        int Sign(float value)
-        {
-            return value > 0 ? 1 : (value < 0 ? -1 : 0);
-        }
     }
 
     public class Pixel
